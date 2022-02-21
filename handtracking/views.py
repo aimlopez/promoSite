715a2103f159b78bc2877de74e6320ee2c70adf2
Aimara Lopez <a.aljorna@gmail.com>
2022-02-21 10:48:06 +0100

    Modify index.html. Add new cards/delete text

--- handtracking/views.py
+++ handtracking/views.py
@@ -10,5 +10,9 @@ def home(request):
     if request.method == 'POST':
         if "hand_tracking" in request.POST:
             os.system('python scripts/handtracking.py')
+        elif "body_posture" in request.POST:
+            os.system('python scripts/bodyposture.py')
+        elif "object_recognition" in request.POST:
+            os.system('python scripts/objectrecognition.py')
 
     return render(request, 'handtracking/index.html')
